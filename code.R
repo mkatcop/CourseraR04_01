54c2f288d6778cef2bf1768a0d3be5bb6e66a484
mkatcop <mitja@kostomaj.net>
2020-04-18 17:17:50 +0100

    working R file for all 4 plots

--- code.R
+++ code.R
@@ -1,2 +1,97 @@
-print("This file was created within RStudio")
-print("And now it lives on GitHub")
+## plot1.R
+
+
+# reading data
+houseHoldPowerDT <- read.table("household_power_consumption.txt", skip = 1, na.strings="?", sep=";")
+# name the columns
+names(houseHoldPowerDT)<- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# new datatable with relevant dates
+relevant_houseHoldPowerDT <- subset(houseHoldPowerDT, houseHoldPowerDT$Date == "1/2/2007" | houseHoldPowerDT$Date == "2/2/2007")
+
+## creating png
+# open the device
+png(file = "plot1.png")
+#create a histogram
+hist(relevant_houseHoldPowerDT$Global_active_power, col = "red", xlab = "Global active power (kilowatts)", main =  "Global active power")
+# close the device
+dev.off()
+
+
+## plot2.R
+
+# reading data
+houseHoldPowerDT <- read.table("household_power_consumption.txt", skip = 1, na.strings="?", sep=";")
+# name the columns
+names(houseHoldPowerDT)<- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# new datatable with relevant dates
+relevant_houseHoldPowerDT <- subset(houseHoldPowerDT, houseHoldPowerDT$Date == "1/2/2007" | houseHoldPowerDT$Date == "2/2/2007")
+
+# new column with proper date format "%d/%m/%Y %H:%M:%S"
+relevant_houseHoldPowerDT$DateTime <- as.POSIXct(paste(relevant_houseHoldPowerDT$Date, relevant_houseHoldPowerDT$Time), format = "%d/%m/%Y %H:%M:%S")
+
+## creating png
+# open the device
+png(file = "plot2.png")
+#create a plot 2
+plot(x = relevant_houseHoldPowerDT$DateTime, y = relevant_houseHoldPowerDT$Global_active_power,type="l", xlab ="", ylab = "Global active power (kilowatts)")
+# close the device
+dev.off()
+
+# plot3.R
+# reading data
+houseHoldPowerDT <- read.table("household_power_consumption.txt", skip = 1, na.strings="?", sep=";")
+# name the columns
+names(houseHoldPowerDT)<- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# new datatable with relevant dates
+relevant_houseHoldPowerDT <- subset(houseHoldPowerDT, houseHoldPowerDT$Date == "1/2/2007" | houseHoldPowerDT$Date == "2/2/2007")
+
+# new column with proper date format "%d/%m/%Y %H:%M:%S"
+relevant_houseHoldPowerDT$DateTime <- as.POSIXct(paste(relevant_houseHoldPowerDT$Date, relevant_houseHoldPowerDT$Time), format = "%d/%m/%Y %H:%M:%S")
+
+## creating png
+# open the device
+png(file = "plot3.png")
+#create a plot 3
+plot(x = relevant_houseHoldPowerDT$DateTime, y = relevant_houseHoldPowerDT$Sub_metering_1,type="l", main = "Energy sub metering", xlab ="", ylab = "Energy sub metering")
+lines(relevant_houseHoldPowerDT$DateTime, relevant_houseHoldPowerDT$Sub_metering_2,type="l",col = "red")
+lines(relevant_houseHoldPowerDT$DateTime, relevant_houseHoldPowerDT$Sub_metering_3,type="l",col = "blue")
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+# close the device
+dev.off()
+
+
+# plot4.R
+# reading data
+houseHoldPowerDT <- read.table("household_power_consumption.txt", skip = 1, na.strings="?", sep=";")
+# name the columns
+names(houseHoldPowerDT)<- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# new datatable with relevant dates
+relevant_houseHoldPowerDT <- subset(houseHoldPowerDT, houseHoldPowerDT$Date == "1/2/2007" | houseHoldPowerDT$Date == "2/2/2007")
+
+# new column with proper date format "%d/%m/%Y %H:%M:%S"
+relevant_houseHoldPowerDT$DateTime <- as.POSIXct(paste(relevant_houseHoldPowerDT$Date, relevant_houseHoldPowerDT$Time), format = "%d/%m/%Y %H:%M:%S")
+
+## creating png
+# open the device
+png(file = "plot4.png")
+# create 2 coloumns and 2 rows for 4 graphs
+par(mfrow = c(2, 2))
+#create a plot 1
+plot(x = relevant_houseHoldPowerDT$DateTime, y = relevant_houseHoldPowerDT$Global_active_power,type="l", xlab ="", ylab = "Global active power")
+# plot 2
+plot(x = relevant_houseHoldPowerDT$DateTime, y = relevant_houseHoldPowerDT$Voltage, type="l", xlab ="", ylab = "Voltage")
+#create a plot 3
+plot(x = relevant_houseHoldPowerDT$DateTime, y = relevant_houseHoldPowerDT$Sub_metering_1,type="l", main = "Energy sub metering", xlab ="", ylab = "Energy sub metering")
+lines(relevant_houseHoldPowerDT$DateTime, relevant_houseHoldPowerDT$Sub_metering_2,type="l",col = "red")
+lines(relevant_houseHoldPowerDT$DateTime, relevant_houseHoldPowerDT$Sub_metering_3,type="l",col = "blue")
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty = 'n', cex=.75)
+#create a plot 4
+plot(x = relevant_houseHoldPowerDT$DateTime, y = relevant_houseHoldPowerDT$Global_reactive_power ,type="l", xlab ="", ylab = "Global reactive power")
+
+
+# close the device
+dev.off()
